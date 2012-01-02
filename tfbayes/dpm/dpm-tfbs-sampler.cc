0f1170dec86c148f8fddd8bd04680df4ab639126
Philipp Benner <philipp.benner@mis.mpg.de>
2012-01-02 10:18:36 +0100

    2012/01/02-10:18:36 (Linux ibmterm01 unknown)

---
+++ tfbayes/dpm/dpm-tfbs-sampler.cc
@@ -0,0 +1,109 @@
+/* Copyright (C) 2011 Philipp Benner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <dpm-tfbs-sampler.hh>
+
+using namespace std;
+using namespace boost::asio;
+using namespace boost::asio::local;
+
+// dpm_tfbs_sampler_t
+////////////////////////////////////////////////////////////////////////////////
+
+dpm_tfbs_sampler_t::dpm_tfbs_sampler_t(
+        mixture_model_t& dpm,
+        dpm_tfbs_state_t& state,
+        const indexer_t& indexer,
+        const string name,
+        bool optimize,
+        save_queue_t<command_t*>& command_queue,
+        save_queue_t<string>& output_queue)
+        : hybrid_sampler_t(dpm, state, indexer, name, optimize),
+          _command_queue(command_queue),
+          _output_queue(output_queue),
+          _state(state) {
+}
+
+bool dpm_tfbs_sampler_t::_sample() {
+        size_t s = hybrid_sampler_t::_sample();
+        while (!_command_queue.empty()) {
+                stringstream ss;
+                command_t* command = _command_queue.front();
+                ss << command->operator()(_state, *this);
+                _command_queue.pop();
+                delete(command);
+                _output_queue.push(ss.str());
+        }
+        return s;
+}
+
+// dpm_tfbs_pmcmc_t
+////////////////////////////////////////////////////////////////////////////////
+
+dpm_tfbs_pmcmc_t::dpm_tfbs_pmcmc_t(
+        const tfbs_options_t& options,
+        const std::vector<std::string>& sequences,
+        size_t n)
+        : population_mcmc_t(n),
+          _data(sequences, options.tfbs_length),
+          _gdpm(n, NULL),
+          _socket_file(options.socket_file),
+          _server(NULL),
+          _bt(NULL) {
+        for (size_t i = 0; i < _size; i++) {
+                std::stringstream ss; ss << "Sampler " << i+1;
+                save_queue_t<command_t*>* command_queue = new save_queue_t<command_t*>();
+                _command_queue.push_back(command_queue);
+                _gdpm[i]       = new dpm_tfbs_t(options, _data);
+                _population[i] = new dpm_tfbs_sampler_t(*_gdpm[i], _gdpm[i]->state(),
+                                                        _data,
+                                                        ss.str(),
+                                                        options.metropolis_optimize,
+                                                        *command_queue,
+                                                        _output_queue);
+        }
+        update_samples();
+        _start_server();
+}
+
+dpm_tfbs_pmcmc_t::~dpm_tfbs_pmcmc_t() {
+        _stop_server();
+}
+
+void
+dpm_tfbs_pmcmc_t::_start_server() {
+        if (_socket_file != "" && _server == NULL) {
+                remove(_socket_file.c_str());
+                _server = new server_t(_ios, _socket_file, _command_queue, _output_queue);
+                _bt     = new boost::thread(boost::bind(&io_service::run, &_ios));
+        }
+}
+
+void
+dpm_tfbs_pmcmc_t::_stop_server() {
+        if (_socket_file != "" && _server != NULL) {
+                _ios.stop();
+                _bt->join();
+                delete(_bt);
+                delete(_server);
+                remove(_socket_file.c_str());
+        }
+}
