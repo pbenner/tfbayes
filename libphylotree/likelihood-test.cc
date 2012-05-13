c78e0d622d6f672b63481b5e2425f68158638a75
Philipp Benner <philipp.benner@mis.mpg.de>
2012-05-14 00:05:32 +0200

    2012/05/14-00:05:31 (Linux thalamus unknown)

--- libphylotree/likelihood-test.cc
+++ libphylotree/likelihood-test.cc
@@ -114,6 +114,46 @@ void test_tree6() {
              << endl << endl;
 }
 
+void test_tree7() {
+        cout << "Test 7:" << endl;
+
+        code_t observations[][2] = {
+                {0, 0}, {0, 0}, {1, 0}
+        };
+        boost::array<double, alphabet_size> p;
+        p[0] = 0.25;
+        p[1] = 0.25;
+        p[2] = 0.25;
+        p[3] = 0.25;
+
+        for (double d = 0.0; d < 1.0; d+=0.01) {
+                double final = 1;
+
+                for (size_t i = 0; i < 3; i++) {
+                        pt_leaf_t n2( observations[i][0], d, "n2");
+                        pt_leaf_t n3( observations[i][1], d, "n3");
+                        pt_root_t n1(-1, &n2, &n3);
+
+                        pt_polynomial_t<code_t, alphabet_size> result(&n1);
+                        final *= result.eval(p);
+                }
+                cout << d << ": " << final
+                     << endl;
+        }
+
+        double gradient = 0;
+        for (size_t i = 0; i < 3; i++) {
+                pt_leaf_t n2( observations[i][0], 0.3, "n2");
+                pt_leaf_t n3( observations[i][1], 0.3, "n3");
+                pt_root_t n1(-1, &n2, &n3);
+
+                pt_polynomial_t<code_t, alphabet_size> result(&n1, &n2);
+                gradient += result.derivative().eval(p)/result.eval(p);
+        }
+        cout << "gradient for n2: " << gradient
+             << endl;
+}
+
 int main(void) {
         test_tree1();
         test_tree2();
@@ -121,6 +161,8 @@ int main(void) {
         test_tree4();
         test_tree5();
         test_tree6();
+
+        test_tree7();
 
         return 0.0;
 }
