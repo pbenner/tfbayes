e8ac44b58d3ffe7536aa0a866597636ec753f305
Philipp Benner <philipp.benner@mis.mpg.de>
2012-12-27 13:33:48 +0100

    2012/12/27-13:33:48 (Darwin sirius.local i386)

--- tfbayes/dpm/dpm_gaussian_interface.py
+++ tfbayes/dpm/dpm_gaussian_interface.py
@@ -14,12 +14,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from ..interface import *
+from tfbayes.interface import *
 
 # load interface
 # ------------------------------------------------------------------------------
 
-_lib = interface.load_library('tfbayes-dpm', 0)
+_lib = load_library('tfbayes-dpm', 0)
 
 # function prototypes
 # ------------------------------------------------------------------------------
