f6167ddbdf23267175533d941ab64a09a7b3c959
Philipp Benner <philipp.benner@mis.mpg.de>
2013-10-07 15:13:05 +0200

    2013/10/07-15:13:05 (Linux x4600-2 x86_64)

--- tfbayes/dpm/dpm-tfbs-interface.cc
+++ tfbayes/dpm/dpm-tfbs-interface.cc
@@ -15,137 +15,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifdef HAVE_CONFIG_H
-#include <tfbayes/config.h>
-#endif /* HAVE_CONFIG_H */
-
-#include <assert.h>
-
-#include <fstream>
-
-#include <tfbayes/alignment/alignment.hh>
-#include <tfbayes/interface/datatypes.hh>
-#include <tfbayes/dpm/init.hh>
-#include <tfbayes/dpm/dpm-tfbs.hh>
-#include <tfbayes/dpm/dpm-tfbs-io.hh>
-#include <tfbayes/dpm/dpm-tfbs-mean.hh>
-#include <tfbayes/dpm/dpm-tfbs-sampler.hh>
-#include <tfbayes/dpm/dpm-partition.hh>
-#include <tfbayes/dpm/utility.hh>
-#include <tfbayes/exception/exception.h>
-#include <tfbayes/utility/linalg.hh>
-
-using namespace std;
-
-__BEGIN_DECLS
-
-// python interface
-// -----------------------------------------------------------------------------
-
-void _dpm_init()
-{
-        __dpm_init__();
-}
-
-dpm_tfbs_pmcmc_t* _dpm_tfbs_init(const tfbs_options_t* options, const sampling_history_t* history)
-{
-        // print options
-        cout << options << endl;
-        // return new sampler
-        return new dpm_tfbs_pmcmc_t(*options, *history);
-}
-
-void _dpm_tfbs_save(dpm_tfbs_pmcmc_t* sampler, const char* filename)
-{
-        if (filename == NULL) {
-                dpm_tfbs_save_result(cout, *sampler);
-        }
-        else {
-                ofstream file;
-                file.open(filename);
-                dpm_tfbs_save_result(file, *sampler);
-                file.close();
-        }
-}
-
-const sampling_history_t* _dpm_tfbs_results(dpm_tfbs_pmcmc_t* sampler)
-{
-        return &sampler->sampling_history();
-}
-
-void _dpm_tfbs_sample(dpm_tfbs_pmcmc_t* sampler, size_t n, size_t burnin) {
-        sampler->sample(n, burnin);
-}
-
-void _dpm_tfbs_free(dpm_tfbs_pmcmc_t* sampler) {
-        delete(sampler);
-}
-
-// handling partitions to resume a previous sampling state
-// -----------------------------------------------------------------------------
-
-dpm_partition_t* _dpm_partition_new()
-{
-        return new dpm_partition_t();
-}
-
-void _dpm_partition_add_component(dpm_partition_t* partition, const char* subset_tag)
-{
-        partition->add_component(subset_tag);
-}
-
-void _dpm_partition_add_index(dpm_partition_t* partition, int sequence, int position)
-{
-        seq_index_t index(sequence, position);
-
-        partition->back().insert(index);
-}
-
-void _dpm_partition_free(dpm_partition_t* partition)
-{
-        delete(partition);
-}
-
-// handling lists of partitions to compute means and medians
-// -----------------------------------------------------------------------------
-
-vector<dpm_partition_t>* _dpm_partition_list_new()
-{
-        return new vector<dpm_partition_t>();
-}
-
-void _dpm_partition_list_add_partition(vector<dpm_partition_t>* partition_list, const dpm_partition_t* partition)
-{
-        partition_list->push_back(*partition);
-}
-
-void _dpm_partition_list_free(vector<dpm_partition_t>* partition_list)
-{
-        delete(partition_list);
-}
-
-// compute point estimates
-// -----------------------------------------------------------------------------
-
-dpm_partition_t* _dpm_map(dpm_tfbs_pmcmc_t* sampler)
-{
-        return new dpm_partition_t(sampler->map());
-}
-
-dpm_partition_t* _dpm_mean(dpm_tfbs_pmcmc_t* sampler)
-{
-        return new dpm_partition_t(sampler->mean());
-}
-
-dpm_partition_t* _dpm_median(dpm_tfbs_pmcmc_t* sampler)
-{
-        return new dpm_partition_t(sampler->median());
-}
-
-__END_DECLS
-
-// python interface
-// -----------------------------------------------------------------------------
+#include <Python.h>
 
 #include <locale>
 #include <cctype>
@@ -156,7 +26,16 @@ __END_DECLS
 #include <boost/python/iterator.hpp>
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
+#include <tfbayes/dpm/init.hh>
+#include <tfbayes/dpm/index.hh>
+#include <tfbayes/dpm/dpm-partition.hh>
+#include <tfbayes/dpm/dpm-sampling-history.hh>
+#include <tfbayes/dpm/dpm-tfbs-options.hh>
+
 using namespace boost::python;
+
+// tools
+// -----------------------------------------------------------------------------
 
 size_t index_i_getitem(index_i& index, size_t i)
 {
@@ -176,8 +55,14 @@ std::string to_string(const T& t)
         return ss.str();
 }
 
+// interface
+// -----------------------------------------------------------------------------
+
 BOOST_PYTHON_MODULE(dpm_tfbs_interface)
 {
+        // init dpm library
+        def("__dpm_init__", __dpm_init__);
+        // class definitions
         class_<sampling_history_t>("sampling_history_t")
                 .def_readwrite("switches",    &sampling_history_t::switches)
                 .def_readwrite("likelihood",  &sampling_history_t::likelihood)
