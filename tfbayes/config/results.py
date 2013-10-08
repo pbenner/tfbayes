2469086990a036a1b3afe8753160292a9d5aa66a
Philipp Benner <philipp.benner@mis.mpg.de>
2013-10-08 09:28:06 +0200

    2013/10/08-09:28:06 (Darwin sirius.local i386)

--- tfbayes/config/results.py
+++ tfbayes/config/results.py
@@ -20,6 +20,16 @@ import ConfigParser
 
 from tools import *
 
+from tfbayes.interface import *
+from tfbayes.dpm.dpm_tfbs_interface import *
+
+# default results config
+# ------------------------------------------------------------------------------
+
+def default_results_config():
+    results_config = sampling_history_t()
+    return results_config
+
 # parse results config
 # ------------------------------------------------------------------------------
 
@@ -29,23 +39,23 @@ def parse_results_config(config_file, results_config):
     if not config_parser.has_section('Result'):
         raise IOError("Invalid configuration file.")
     if config_parser.has_option('Result', 'components'):
-        results_config['components'] = read_matrix(config_parser, 'Result', 'components', int)
+        results_config.components = read_matrix(config_parser, 'Result', 'components', int)
     if config_parser.has_option('Result', 'likelihood'):
-        results_config['likelihood'] = read_matrix(config_parser, 'Result', 'likelihood', float)
+        results_config.likelihood = read_matrix(config_parser, 'Result', 'likelihood', float)
     if config_parser.has_option('Result', 'posterior'):
-        results_config['posterior'] = read_matrix(config_parser, 'Result', 'posterior', float)
+        results_config.posterior = read_matrix(config_parser, 'Result', 'posterior', float)
     if config_parser.has_option('Result', 'temperature'):
-        results_config['temperature'] = read_matrix(config_parser, 'Result', 'temperature', float)
+        results_config.temperature = read_matrix(config_parser, 'Result', 'temperature', float)
     if config_parser.has_option('Result', 'switches'):
-        results_config['switches'] = read_matrix(config_parser, 'Result', 'switches', float)
+        results_config.switches = read_matrix(config_parser, 'Result', 'switches', float)
     if config_parser.has_option('Result', 'map_partition'):
-        results_config['map_partition'] = config_parser.get('Result', 'map_partition')
+        results_config.map_partition = config_parser.get('Result', 'map_partition')
     if config_parser.has_option('Result', 'mean_partition'):
-        results_config['mean_partition'] = config_parser.get('Result', 'mean_partition')
+        results_config.mean_partition = config_parser.get('Result', 'mean_partition')
     if config_parser.has_option('Result', 'median_partition'):
-        results_config['median_partition'] = config_parser.get('Result', 'median_partition')
+        results_config.median_partition = config_parser.get('Result', 'median_partition')
     if config_parser.has_option('Result', 'partitions'):
-        results_config['partitions'] = config_parser.get('Result', 'partitions')
+        results_config.partitions = config_parser.get('Result', 'partitions')
 
 # save results config
 # ------------------------------------------------------------------------------
@@ -53,18 +63,18 @@ def parse_results_config(config_file, results_config):
 def save_results_config(config_file, results_config):
     config_parser = ConfigParser.RawConfigParser()
     config_parser.add_section('Result')
-    write_matrix(config_parser, 'Result', 'components',  results_config['components'])
-    write_matrix(config_parser, 'Result', 'likelihood',  results_config['likelihood'])
-    write_matrix(config_parser, 'Result', 'posterior',   results_config['posterior'])
-    write_matrix(config_parser, 'Result', 'switches',    results_config['switches'])
-    write_matrix(config_parser, 'Result', 'temperature', results_config['temperature'])
+    write_matrix(config_parser, 'Result', 'components',  results_config.components)
+    write_matrix(config_parser, 'Result', 'likelihood',  results_config.likelihood)
+    write_matrix(config_parser, 'Result', 'posterior',   results_config.posterior)
+    write_matrix(config_parser, 'Result', 'switches',    results_config.switches)
+    write_matrix(config_parser, 'Result', 'temperature', results_config.temperature)
     if results_config.has_key('partitions'):
-        config_parser.set('Result', 'partitions',       results_config['partitions'])
+        config_parser.set('Result', 'partitions',       results_config.partitions)
     if results_config.has_key('map_partition'):
-        config_parser.set('Result', 'map_partition',    results_config['map_partition'])
+        config_parser.set('Result', 'map_partition',    results_config.map_partition)
     if results_config.has_key('mean_partition'):
-        config_parser.set('Result', 'mean_partition',   results_config['mean_partition'])
+        config_parser.set('Result', 'mean_partition',   results_config.mean_partition)
     if results_config.has_key('median_partition'):
-        config_parser.set('Result', 'median_partition', results_config['median_partition'])
+        config_parser.set('Result', 'median_partition', results_config.median_partition)
     with open(config_file, 'wb') as config_fp:
         config_parser.write(config_fp)
--- tfbayes/interface/datatypes.cc
+++ tfbayes/interface/datatypes.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Philipp Benner
+/* Copyright (C) 2013 Philipp Benner
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,108 +15,165 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <iostream>
-#include <tfbayes/interface/datatypes.hh>
+#include <locale>
+#include <cctype>
+#include <list>
+#include <string>
 
-using namespace std;
+#include <boost/python/module.hpp>
+#include <boost/python/tuple.hpp>
+#include <boost/python/init.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/make_constructor.hpp>
+#include <boost/python/implicit.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
-__BEGIN_DECLS
+#include <tfbayes/utility/linalg.hh>
 
-// c++ strings
-////////////////////////////////////////////////////////////////////////////////
+using namespace boost::python;
 
-string* _cxx_string_alloc(const char* str)
+void raise_IndexError()
 {
-        string* ptr = new string(str);
-        return ptr;
+        PyErr_SetString(PyExc_IndexError, "Index out of range");
+        throw error_already_set();
 }
 
-void _cxx_string_free(string* ptr)
-{
-        delete(ptr);
-}
+// vector class
+// -----------------------------------------------------------------------------
 
-const char* _cxx_string_getstr(string* ptr)
+std::vector<double>* vector_from_list(list& ns)
 {
-        return ptr->c_str();
-}
+        std::vector<double>* v = new std::vector<double>(len(ns), 0);
 
-// c++ vector/matrix interface
-////////////////////////////////////////////////////////////////////////////////
-
-vector<double>* _cxx_vector_alloc(size_t size)
-{
-        vector<double>* ptr = new vector<double>(size, 0.0);
-        return ptr;
-}
-
-matrix<double>* _cxx_matrix_alloc(size_t rows, size_t columns)
-{
-        matrix<double>* ptr = new matrix<double>(rows, columns);
-        return ptr;
-}
-
-size_t _cxx_vector_size(vector<double>* vector)
-{
-        return vector->size();
-}
-
-size_t _cxx_matrix_rows(matrix<double>* matrix)
-{
-        return matrix->size();
-}
-
-size_t _cxx_matrix_columns(matrix<double>* matrix)
-{
-        if (matrix->size() == 0) {
-                return 0;
+        for (ssize_t i = 0; i < len(ns); i++) {
+                (*v)[i] = extract<double>(ns[i]);
         }
-        else {
-                return matrix->operator[](0).size();
+        return v;
+}
+
+list vector_to_list(std::vector<double>& v)
+{
+        object get_iter = iterator<std::vector<double> >();
+        object iter = get_iter(v);
+        list l(iter);
+        return l;
+}
+
+// matrix class
+// -----------------------------------------------------------------------------
+
+std::matrix<double>* matrix_from_list(list& ns)
+{
+        std::matrix<double>* m = new std::matrix<double>();
+
+        for (ssize_t i = 0; i < len(ns); i++) {
+                list l = extract<list>(ns[i]);
+                m->push_back(std::vector<double>(len(l), 0.0));
+                for (ssize_t j = 0; j < len(l); j++) {
+                        (*m)[i][j] = extract<double>(l[j]);
+                }
         }
+        return m;
 }
 
-double _cxx_vector_read(vector<double>* vector, size_t pos)
+list matrix_to_list(std::matrix<double>& m)
 {
-        return vector->operator[](pos);
+        list l;
+        for (size_t i = 0; i < m.size(); i++) {
+                object get_iter = iterator<std::vector<double> >();
+                object iter = get_iter(m[i]);
+                l.append(list(iter));
+        }
+        return l;
 }
 
-double _cxx_matrix_read(matrix<double>* matrix, size_t row, size_t column)
+double matrix_getitem(std::matrix<double>& m, tuple index)
 {
-        return matrix->operator[](row)[column];
+        size_t i = extract<size_t>(index[0]);
+        size_t j = extract<size_t>(index[1]);
+        if (i >= m   .size()) raise_IndexError();
+        if (j >= m[i].size()) raise_IndexError();
+        return m[i][j];
 }
 
-void _cxx_vector_write(vector<double>* vector, size_t pos, double d)
+void matrix_setitem(std::matrix<double>& m, tuple index, double d)
 {
-        vector->operator[](pos) = d;
+        size_t i = extract<size_t>(index[0]);
+        size_t j = extract<size_t>(index[1]);
+        if (i >= m   .size()) raise_IndexError();
+        if (j >= m[i].size()) raise_IndexError();
+        m[i][j] = d;
 }
 
-void _cxx_matrix_write(matrix<double>* matrix, size_t row, size_t column, double d)
+// implicit type conversion
+// -----------------------------------------------------------------------------
+
+struct iterable_converter
 {
-        matrix->operator[](row)[column] = d;
-}
+        /// @note Registers converter from a python interable type to the
+        ///       provided type.
+        template <typename Container>
+        iterable_converter&
+        from_python() {
+                boost::python::converter::registry::push_back(
+                        &iterable_converter::convertible,
+                        &iterable_converter::construct<Container>,
+                        boost::python::type_id<Container>());
+                return *this;
+        }
+        static void* convertible(PyObject* object) {
+                return PyObject_GetIter(object) ? object : NULL;
+        }
+        template <typename Container>
+        static void construct(
+                PyObject* object,
+                boost::python::converter::rvalue_from_python_stage1_data* data) {
+                namespace python = boost::python;
+                // Object is a borrowed reference, so create a handle indicting it is
+                // borrowed for proper reference counting.
+                python::handle<> handle(python::borrowed(object));
+                
+                // Obtain a handle to the memory block that the converter has allocated
+                // for the C++ type.
+                typedef python::converter::rvalue_from_python_storage<Container>
+                        storage_type;
+                void* storage = reinterpret_cast<storage_type*>(data)->storage.bytes;
+                
+                typedef python::stl_input_iterator<typename Container::value_type>
+                        iterator;
+                
+                // Allocate the C++ type into the converter's memory block, and assign
+                // its handle to the converter's convertible variable.  The C++
+                // container is populated by passing the begin and end iterators of
+                // the python object to the container's constructor.
+                data->convertible = new (storage) Container(
+                        iterator(python::object(handle)), // begin
+                        iterator());                      // end
+        }
+};
 
-void _cxx_vector_free(std::vector<double>* vector)
+// register classes
+// -----------------------------------------------------------------------------
+
+BOOST_PYTHON_MODULE(linalg)
 {
-        delete(vector);
+        class_<std::vector<double> >("vector")
+                .def(vector_indexing_suite<std::vector<double> >() )
+                .def("__init__", make_constructor(vector_from_list))
+                .def("export", vector_to_list)
+                ;
+        class_<std::matrix<double> >("matrix")
+                .def(vector_indexing_suite<std::matrix<double> >() )
+                .def("__init__", make_constructor(matrix_from_list))
+                .def("__getitem__", matrix_getitem)
+                .def("__setitem__", matrix_setitem)
+                .def("export", matrix_to_list)
+                ;
+      iterable_converter()
+              .from_python<std::vector<double> >()
+              .from_python<std::matrix<double> >()
+              .from_python<std::list<std::string> >()
+              .from_python<std::list<std::matrix<double > > >()
+              ;
 }
-
-void _cxx_matrix_free(std::matrix<double>* matrix)
-{
-        delete(matrix);
-}
-
-// vector interface
-////////////////////////////////////////////////////////////////////////////////
-
-vector_t * _alloc_vector(size_t size) {
-        return alloc_vector(size);
-}
-matrix_t * _alloc_matrix(size_t rows, size_t columns) {
-        return alloc_matrix(rows, columns);
-}
-void _free_vector(vector_t *v) { free_vector(v); }
-void _free_matrix(matrix_t *m) { free_matrix(m); }
-void _free(void *ptr)          { free(ptr); }
-
-__END_DECLS
