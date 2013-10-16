99bfc7d7b682776be60a3a0356cdcdc62af8d75c
Philipp Benner <philipp.benner@mis.mpg.de>
2013-10-16 21:46:22 +0200

    2013/10/16-21:46:22 (Linux thalamus unknown)

--- tfbayes/interface/sigint.py
+++ tfbayes/interface/sigint.py
@@ -14,4 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from datatypes import *
+# allow to send ctrl-c to the library
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
