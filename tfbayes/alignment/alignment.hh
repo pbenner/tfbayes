0dd183d75f1d7020321ce214e3c12a725af06886
Philipp Benner <philipp.benner@mis.mpg.de>
2013-10-16 19:59:38 +0200

    2013/10/16-19:59:38 (Linux thalamus unknown)

--- tfbayes/alignment/alignment.hh
+++ tfbayes/alignment/alignment.hh
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012 Philipp Benner
+/* Copyright (C) 2012-2013 Philipp Benner
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
