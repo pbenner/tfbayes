f1b8c791b31a6fb461b7fc37f2dfe3b8b82eddd7
Philipp Benner <philipp.benner@mis.mpg.de>
2013-11-06 10:05:03 +0100

    2013/11/06-10:05:03 (Linux x4600-2 x86_64)

--- tfbayes/fg/mailbox.hh
+++ tfbayes/fg/mailbox.hh
@@ -52,6 +52,16 @@ protected:
         const T* _ptr_;
         boost::function<void ()> _fptr_;
 };
+
+// For variable nodes, it is necessary that the inbox is able to have
+// as many slots as factor nodes are connected to the variable nodes,
+// however, this might vary and is not determined by the type of the
+// variable node. Factor nodes on the other hand, have a fixed number
+// of slots that depends on the type of the node. If a variable node
+// is connected to a slot, it receives a link to this slot which is
+// added to the outbox of the variable node. If no variable node is
+// connected to the slot, then a dirac distribution is placed in the
+// slot that represents the respective fixed parameter.
 template <typename T>
 class _inbox_t : public std::vector<boost::optional<mailbox_slot_t<T>&> > {
 public:
@@ -71,6 +81,12 @@ public:
                 }
         }
 };
+
+// An outbox has a fixed number of slots for factor nodes. If a
+// variable node connects to the factor, the respective slot of the
+// outbox points to the desired mailbox slot of the connecting node.
+// Variable nodes push as many slots to the outbox as needed, the
+// number is not determined by the type of the variable node.
 template <typename T>
 class outbox_t : public std::vector<boost::optional<mailbox_slot_t<T>&> > {
 public:
