d0988dd48fed29a862636d00b8075c43fcae0e2c
Philipp Benner <philipp.benner@mis.mpg.de>
2013-11-16 21:35:31 +0100

    2013/11/16-21:35:31 (Linux thalamus unknown)

--- tfbayes/fg/factor-graph-test.py
+++ tfbayes/fg/factor-graph-test.py
@@ -61,6 +61,27 @@ def plot_density(ax, d, x_limits, xlab="", ylab="", n=1001):
     ax.set_ylabel(ylab)
     return ax.plot(x,y)
 
+def plot_fg(fg, bound):
+    # obtain estimates
+    e_mu  = fg["v2"].moments(0)
+    e_tau = fg["v3"].moments(1)
+    d     = normal_distribution_t(e_mu, e_tau)
+    d1    = fg["v2"]
+    d2    = fg["v3"]
+    # plot result
+    plt.clf()
+    ax1 = plt.subplot2grid((3,2), (0,0), colspan=2)
+    ax2 = plt.subplot2grid((3,2), (1,0))
+    ax3 = plt.subplot2grid((3,2), (1,1))
+    ax4 = plt.subplot2grid((3,2), (2,0), colspan=2)
+    plot_density(ax1, d,  [ 0.5,  1.5], xlab="x", ylab="density")
+    plot_density(ax2, d1, [ 0.9,  1.1], xlab=r'$\mu$', ylab="density")
+    plot_density(ax3, d2, [50, 90], xlab=r'$\tau$')
+    ax1.hist(data, 50, normed=1)
+    ax4.plot(bound)
+    #plt.savefig("factor-graph-test-1.png")
+    plt.show()
+
 # test 1
 ################################################################################
 
@@ -86,23 +107,6 @@ data  = np.random.normal(mu, sigma, 1000)
 
 # construct and execute the factor graph
 fg = construct_fg1(data, threads=10)
-fg()
+bound = fg()
 
-# obtain estimates
-e_mu  = fg["v2"].moments(0)
-e_tau = fg["v3"].moments(1)
-d     = normal_distribution_t(e_mu, e_tau)
-d1    = fg["v2"]
-d2    = fg["v3"]
-
-# plot result
-plt.clf()
-ax1 = plt.subplot2grid((2,2), (0,0), colspan=2)
-ax2 = plt.subplot2grid((2,2), (1,0))
-ax3 = plt.subplot2grid((2,2), (1,1))
-plot_density(ax1, d,  [ 0.5,  1.5], xlab="x", ylab="density")
-plot_density(ax2, d1, [ 0.9,  1.1], xlab=r'$\mu$', ylab="density")
-plot_density(ax3, d2, [50, 90], xlab=r'$\tau$')
-ax1.hist(data, 50, normed=1)
-#plt.savefig("factor-graph-test-1.png")
-plt.show()
+plot_fg(fg, bound)
