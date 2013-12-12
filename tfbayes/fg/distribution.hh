d2cb2c42eca98317aeb58d49be0b3b98245bc2a3
Philipp Benner <philipp.benner@mis.mpg.de>
2013-12-12 09:16:22 +0100

    2013/12/12-09:16:21 (Linux thalamus unknown)

--- tfbayes/fg/distribution.hh
+++ tfbayes/fg/distribution.hh
@@ -654,14 +654,15 @@ public:
                 }
                 return h;
         }
-        virtual statistics_t statistics(const vector_t& x) const {
+        virtual statistics_t statistics(double x) const {
                 statistics_t T(k());
-                for (size_t i = 0; i < x.size(); i++) {
-                        assert(x[i] < k());
-                        T[x[i]] += 1.0;
-                }
-                T.n = 1;
+                assert(x < k());
+                T[x] += 1.0;
+                T.n   = 1;
                 return T;
+        }
+        virtual statistics_t statistics(const vector_t& x) const {
+                return statistics(x[0]);
         }
 };
 
