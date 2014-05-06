573072c86662f16b215117c4b1baffffb943209f
Philipp Benner <philipp.benner@mis.mpg.de>
2014-05-06 17:57:55 +0200

    2014/05/06-17:57:55 (Darwin sirius.local i386)

---
+++ tfbayes/phylotree/expected-terms.R
@@ -0,0 +1,27 @@
+# required libraries
+################################################################################
+require(tikzDevice)
+
+################################################################################
+
+t <- read.table("expected-terms.dat", header=F)
+colnames(t) <- c("x", "y1", "y2")
+attach(t)
+
+plot(x, y1, type='l',
+     xlab="Number of vertices",
+     ylab="Expected number of terms")
+lines(x, y2, lty=2)
+legend("bottomright", c("pruning algorithm", "likelihood decomposition"), lty=c(1,2), bty='n')
+
+# export
+################################################################################
+
+tikz("expected-terms.tex", bareBones=TRUE, width=3.2, height=2.3)
+par(mar=c(4.2,4,1,1))
+plot(x, y1, type='l',
+     xlab="Number of vertices",
+     ylab="Expected number of terms")
+lines(x, y2, lty=2)
+legend("top", c("pruning", "decomposition"), lty=c(1,2), bty='n')
+dev.off()
