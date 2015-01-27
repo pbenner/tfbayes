7afcbdcd1bf361f417d12e4cee24f12417965681
Philipp Benner <philipp.benner@mis.mpg.de>
2015-01-27 16:57:39 +0100

    2015/01/27-16:57:38 (Linux thalamus unknown)

---
+++ tfbayes/utility/probability-test.cc
@@ -0,0 +1,32 @@
+/* Copyright (C) 2015 Philipp Benner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <tfbayes/utility/probability.hh>
+
+using namespace std;
+
+int
+main(void)
+{
+        probability_t a(0.3);
+        probability_t b(0.2);
+
+        cout << "a + b = " << a+b << endl;
+        cout << "a - b = " << a-b << endl;
+        cout << "a * b = " << a*b << endl;
+        cout << "a / b = " << a/b << endl;
+}
---
+++ tfbayes/utility/probability.hh
@@ -0,0 +1,91 @@
+/* Copyright (C) 2015 Philipp Benner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __TFBAYES_UTILITY_PROBABILITY_HH__
+#define __TFBAYES_UTILITY_PROBABILITY_HH__
+
+#ifdef HAVE_CONFIG_H
+#include <tfbayes/config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <iostream>
+#include <cmath>
+
+#include <tfbayes/utility/logarithmetic.hh>
+
+class probability_t {
+public:
+        probability_t()
+        : log_p(-std::numeric_limits<double>::infinity())
+                { }
+        probability_t(double p)
+        : log_p(std::log(p))
+                { }
+        probability_t(const probability_t& p)
+        : log_p(p.log_p)
+                { }
+
+        probability_t& operator+=(const probability_t& p) {
+                log_p = logadd(log_p, p.log_p);
+                return *this;
+        }
+        probability_t& operator-=(const probability_t& p) {
+                log_p = logsub(log_p, p.log_p);
+                return *this;
+        }
+        probability_t& operator*=(const probability_t& p) {
+                log_p += p.log_p;
+                return *this;
+        }
+        probability_t& operator/=(const probability_t& p) {
+                log_p -= p.log_p;
+                return *this;
+        }
+
+        friend
+        std::ostream& operator<< (std::ostream& o, const probability_t& p) {
+                if (p.log_p == -std::numeric_limits<double>::infinity()) {
+                        o << 0.0;
+                }
+                else {
+                        o << std::exp(p.log_p);
+                }
+                return o;
+        }
+
+protected:
+        double log_p;
+};
+
+probability_t operator+(const probability_t& p, const probability_t& q) {
+        probability_t tmp(p);
+        return tmp += q;
+}
+probability_t operator-(const probability_t& p, const probability_t& q) {
+        probability_t tmp(p);
+        return tmp -= q;
+}
+probability_t operator*(const probability_t& p, const probability_t& q) {
+        probability_t tmp(p);
+        return tmp *= q;
+}
+probability_t operator/(const probability_t& p, const probability_t& q) {
+        probability_t tmp(p);
+        return tmp /= q;
+}
+
+#endif /* __TFBAYES_UTILITY_PROBABILITY_HH__ */
