03fd40406342ccf96575e9c9a21a5286fc30822a
Philipp Benner <philipp.benner@mis.mpg.de>
2015-01-29 16:40:47 +0100

    2015/01/29-16:40:46 (Linux x4600-5 x86_64)

--- tfbayes/entropy/entropy-distribution-test.R
+++ tfbayes/entropy/entropy-distribution-test.R
@@ -10,6 +10,12 @@ a2 <- 20
 # execute sampler
 system(sprintf("./entropy-distribution-test %d %d %d > entropy-distribution-test.csv", n, a1, a2))
 # load result
-theta <- read.table("entropy-distribution-test.csv")
+samples <- read.table("entropy-distribution-test.csv")
+samples.entropy <- apply(samples, 1, entropy)
 
 simplex.contour(theta, filled=TRUE)
+
+hist(samples.entropy, 100, prob=T, xlim=c(0, log(3)))
+par(new=TRUE)
+curve(dentropy(x, a1, a2), 0, log(3), xaxt="n", yaxt="n")
+axis(4)
