b37b8de2520b062197a4f767a1688476e00a368b
Philipp Benner <philipp.benner@mis.mpg.de>
2015-01-30 21:43:38 +0100

    2015/01/30-21:43:37 (Linux thalamus unknown)

--- tfbayes/entropy/entropy-approximation.cc
+++ tfbayes/entropy/entropy-approximation.cc
@@ -30,6 +30,10 @@
 #include <tfbayes/utility/distribution.hh>
 #include <tfbayes/entropy/entropy.hh>
 
+typedef histogram_t<double, probability_t> prob_histogram_t;
+
+BOOST_CLASS_VERSION(prob_histogram_t, 1)
+
 template <typename T>
 void seed_rng(T& rng)
 {
@@ -38,13 +42,13 @@ void seed_rng(T& rng)
         rng.seed(tv.tv_sec*tv.tv_usec);
 }
 
-histogram_t<double, probability_t>
+prob_histogram_t
 approximate_distribution(size_t k, size_t n, size_t bins)
 {
         boost::random::mt19937 gen; seed_rng(gen);
         std::vector<double> alpha(k, 1.0);
         boost::random::dirichlet_distribution<double, probability_t> dist(alpha);
-        histogram_t<double, probability_t> histogram(0.0, std::log(k), bins);
+        prob_histogram_t histogram(0.0, std::log(k), bins);
 
         for (size_t i = 0; i < n; i++) {
                 histogram.add(entropy(dist(gen)));
@@ -64,7 +68,7 @@ main(void)
                 cerr << boost::format("Sampling entropies on simplices of dimension %d...") % k
                      << endl;
 
-                const histogram_t<double, probability_t> histogram = approximate_distribution(k, n[k], 100);
+                const prob_histogram_t histogram = approximate_distribution(k, n[k], 100);
 
                 // save pdf to table
                 {
