6441c5592966d4bbdd7a70c08638fa99a407fe6e
Philipp Benner <philipp.benner@mis.mpg.de>
2015-02-01 12:02:21 +0100

    2015/02/01-12:02:21 (Linux x4600-5 x86_64)

--- tfbayes/entropy/entropy-distribution-test.R
+++ tfbayes/entropy/entropy-distribution-test.R
@@ -4,25 +4,26 @@ source("entropy-plot.R")
 
 ################################################################################
 
-dentropy <- function(h, a1 = 100, a2 = 20, ...) {
-  dbeta(h/log(3), shape1 = a1, shape2 = a2)/log(3)
+dentropy <- function(h, k, a1 = 100, a2 = 20, ...) {
+  dbeta(h/log(k), shape1 = a1, shape2 = a2)/log(3)
 }
 
 ################################################################################
 
 n  <- 100000
+k  <- 3
 a1 <- 100
 a2 <- 20
 
 # execute sampler
-system(sprintf("./entropy-distribution-test %d 2 %d %d > entropy-distribution-test.csv", n, a1, a2))
+system(sprintf("./entropy-distribution-test %d %d %d %d > entropy-distribution-test.csv", n, k, a1, a2))
 # load result
 samples <- read.table("entropy-distribution-test.csv")
 samples.entropy <- apply(samples, 1, entropy)
 
 simplex.contour(samples, filled=TRUE)
 
-hist(samples.entropy, 100, prob=T, xlim=c(0, log(3)))
+hist(samples.entropy, 100, prob=T, xlim=c(0, log(k)))
 par(new=TRUE)
-curve(dentropy(x, a1, a2), 0, log(3), xaxt="n", yaxt="n")
+curve(dentropy(x, k, a1, a2), 0, log(k), xaxt="n", yaxt="n")
 axis(4)
