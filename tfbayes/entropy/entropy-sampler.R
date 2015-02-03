81f239e24d614d1d75d9c48b489fa960d91262a5
Philipp Benner <philipp.benner@mis.mpg.de>
2015-02-03 16:10:12 +0100

    2015/02/03-16:10:12 (Linux x4600-5 x86_64)

--- tfbayes/entropy/entropy-sampler.R
+++ tfbayes/entropy/entropy-sampler.R
@@ -10,7 +10,7 @@ source("entropy.R")
 # simplex sampler for a general density f
 ################################################################################
 
-simplex.sampler.loop.dir <- function(n, f, k) {
+simplex.sampler.dir <- function(n, f, k) {
   alpha <- rep(1, k)
   rdir  <- function( ) c(rdirichlet(1, alpha))
   ddir  <- function(x)   ddirichlet(x, alpha)
@@ -32,7 +32,7 @@ simplex.sampler.loop.dir <- function(n, f, k) {
   result
 }
 
-simplex.sampler.loop.norm <- function(n, f, k, sd=0.05, ...) {
+simplex.sampler.norm <- function(n, f, k, sd=0.05, ...) {
   stopifnot(k > 1)
   rprop  <- function(qj) rnorm(1, mean=qj, sd=sd)
   # initial state
@@ -75,10 +75,62 @@ simplex.sampler.loop.norm <- function(n, f, k, sd=0.05, ...) {
   result
 }
 
+simplex.sampler.ham.grad <- function(q) {
+  n <- length(q)
+  g <- rep(0.0, n)
+  for (i in 1:(n-1)) {
+    g[i] <- log(q[n]) - log(q[i])
+  }
+  g
+}
+
+simplex.sampler.ham <- function(n, f, k, epsilon=0.01, L=10, ...) {
+  stopifnot(k > 1)
+  # initial state
+  result <- rbind(NULL, rdirichlet(1, rep(1, k)))
+  for (i in 1:n) {
+    current_q <- result[i,]
+    current_p <- c(rnorm(k-1, 0, 1), 0)
+    q <- current_q
+    p <- current_p
+    r <- TRUE
+    # make a half step for the momentum
+    p <- p - epsilon*simplex.sampler.ham.grad(q)
+    for (l in 1:L) {
+      # make a full step for the position
+      q <- q + epsilon*p
+      # renormalize
+      q[k] <- 1-sum(q[-k])
+      # check if still within the probability simplex
+      if (q[k] < 0.0) {
+        continue
+      }
+    }
+    # accept or reject
+    if (f(q) == 0 || runif(1) <= min(1, f(p)/f(q))) {
+      q <- p
+      r <- FALSE
+    }
+    # propose a permutation
+    p <- gtools::permute(q)
+    # save result
+    result <- rbind(result, q)
+    # mark if sample was rejected
+    if (r) {
+      row.names(result)[i+1] <- "x"
+    }
+    else {
+      row.names(result)[i+1] <- ""
+    }
+  }
+  result
+}
+
 simplex.sampler <- function(n, f, k, burnin=1000, method="normal", ...) {
   result <- switch(method,
-                   normal    = simplex.sampler.loop.norm(n+burnin, f, k, ...),
-                   dirichlet = simplex.sampler.loop.dir (n+burnin, f, k, ...))
+                   normal    = simplex.sampler.norm(n+burnin, f, k, ...),
+                   hamilton  = simplex.sampler.ham (n+burnin, f, k, ...),
+                   dirichlet = simplex.sampler.dir (n+burnin, f, k, ...))
   result <- result[(burnin+1):(n+burnin),]
   print (sprintf("Acceptance rate: %f", sum(row.names(result) != "x")/n))
   result
