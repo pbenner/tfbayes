a552f1fd85c840f6be09ab479daf6d1a97db40a6
Philipp Benner <philipp.benner@mis.mpg.de>
2015-02-15 14:09:03 +0100

    2015/02/15-14:09:03 (Linux thalamus unknown)

--- tfbayes/entropy/entropy-approximation.hh
+++ tfbayes/entropy/entropy-approximation.hh
@@ -28,29 +28,33 @@
 
 #include <tfbayes/utility/histogram.hh>
 
-#include <tfbayes/entropy/entropy-approximation-2.hh>
-#include <tfbayes/entropy/entropy-approximation-3.hh>
-#include <tfbayes/entropy/entropy-approximation-4.hh>
-#include <tfbayes/entropy/entropy-approximation-5.hh>
-#include <tfbayes/entropy/entropy-approximation-6.hh>
-#include <tfbayes/entropy/entropy-approximation-7.hh>
-#include <tfbayes/entropy/entropy-approximation-8.hh>
-#include <tfbayes/entropy/entropy-approximation-9.hh>
-#include <tfbayes/entropy/entropy-approximation-10.hh>
-
 template <class input_type, class result_type>
 histogram_t<input_type, result_type> entropy_approximation(size_t k)
 {
+        typedef histogram_t<input_type, result_type> hist_t;
+
+        // load data
+        #include <tfbayes/entropy/entropy-approximation-2.hh>
+        #include <tfbayes/entropy/entropy-approximation-3.hh>
+        #include <tfbayes/entropy/entropy-approximation-4.hh>
+        #include <tfbayes/entropy/entropy-approximation-5.hh>
+        #include <tfbayes/entropy/entropy-approximation-6.hh>
+        #include <tfbayes/entropy/entropy-approximation-7.hh>
+        #include <tfbayes/entropy/entropy-approximation-8.hh>
+        #include <tfbayes/entropy/entropy-approximation-9.hh>
+        #include <tfbayes/entropy/entropy-approximation-10.hh>
+
+        // construct and return the histogram
         switch(k) {
-        case  2: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_2,  entropy_histogram_2_counts);
-        case  3: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_3,  entropy_histogram_3_counts);
-        case  4: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_4,  entropy_histogram_4_counts);
-        case  5: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_5,  entropy_histogram_5_counts);
-        case  6: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_6,  entropy_histogram_6_counts);
-        case  7: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_7,  entropy_histogram_7_counts);
-        case  8: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_8,  entropy_histogram_8_counts);
-        case  9: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_9,  entropy_histogram_9_counts);
-        case 10: return histogram_t<input_type, result_type>(0.0, std::log(k), entropy_histogram_10, entropy_histogram_10_counts);
+        case  2: return hist_t(0.0, std::log(k), entropy_histogram_2,  entropy_histogram_2_counts);
+        case  3: return hist_t(0.0, std::log(k), entropy_histogram_3,  entropy_histogram_3_counts);
+        case  4: return hist_t(0.0, std::log(k), entropy_histogram_4,  entropy_histogram_4_counts);
+        case  5: return hist_t(0.0, std::log(k), entropy_histogram_5,  entropy_histogram_5_counts);
+        case  6: return hist_t(0.0, std::log(k), entropy_histogram_6,  entropy_histogram_6_counts);
+        case  7: return hist_t(0.0, std::log(k), entropy_histogram_7,  entropy_histogram_7_counts);
+        case  8: return hist_t(0.0, std::log(k), entropy_histogram_8,  entropy_histogram_8_counts);
+        case  9: return hist_t(0.0, std::log(k), entropy_histogram_9,  entropy_histogram_9_counts);
+        case 10: return hist_t(0.0, std::log(k), entropy_histogram_10, entropy_histogram_10_counts);
         default: throw std::runtime_error("Invalid cardinality!");
         }
 }
