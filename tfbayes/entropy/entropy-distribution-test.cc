3de051c455dcd5b135cc4f2bccbccef48398cc88
Philipp Benner <philipp.benner@mis.mpg.de>
2015-02-16 11:56:56 +0100

    2015/02/16-11:56:56 (Linux x4600-5 x86_64)

--- tfbayes/entropy/entropy-distribution-test.cc
+++ tfbayes/entropy/entropy-distribution-test.cc
@@ -18,11 +18,14 @@
 #include <iostream>
 #include <vector>
 
+#include <getopt.h>
+
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #include <tfbayes/utility/probability.hh>
 #include <tfbayes/entropy/entropy-distribution.hh>
+#include <tfbayes/utility/strtools.hh>
 
 typedef probability_t<> p_t;
 typedef std::vector<p_t> p_vector_t;
@@ -35,6 +38,73 @@ std::ostream& operator<<(std::ostream& o, const p_vector_t& v) {
         return o;
 }
 
+using namespace std;
+
+// options
+////////////////////////////////////////////////////////////////////////////////
+
+struct options_t {
+        double variance;
+        double a1;
+        double a2;
+        bool print_entropy;
+        bool verbose;
+        options_t()
+                : variance      (0.1),
+                  a1            (1.0),
+                  a2            (1.0),
+                  print_entropy (false),
+                  verbose       (false)
+                { }
+};
+
+static options_t options;
+
+// usage and version information
+////////////////////////////////////////////////////////////////////////////////
+
+static
+void print_usage(char *pname, FILE *fp)
+{
+        (void)fprintf(fp, "\nUsage: %s [OPTION] SAMPLES CARDINALITY\n\n", pname);
+        (void)fprintf(fp,
+                      "Options:\n"
+                      "      --counts=A1:A2            - pseudocounts for the beta prior\n"
+                      "      --print-entropy           - print entropy instead of the distributions\n"
+                      "      --variance=float          - variance of the proposal distribution\n"
+                      "   -v                           - be verbose\n"
+                      "\n"
+                      "      --help                    - print help and exit\n"
+                      "      --version                 - print version information and exit\n\n");
+}
+
+static
+void wrong_usage(char *pname, const char *msg)
+{
+
+        if(msg != NULL) {
+                (void)fprintf(stderr, "%s\n", msg);
+        }
+        (void)fprintf(stderr,
+                      "Try `%s --help' for more information.\n", pname);
+
+        exit(EXIT_FAILURE);
+
+}
+
+static
+void print_version(FILE *fp)
+{
+        (void)fprintf(fp,
+                      "This is free software, and you are welcome to redistribute it\n"
+                      "under certain conditions; see the source for copying conditions.\n"
+                      "There is NO warranty; not even for MERCHANTABILITY or FITNESS\n"
+                      "FOR A PARTICULAR PURPOSE.\n\n");
+}
+
+// main program
+////////////////////////////////////////////////////////////////////////////////
+
 template <typename T>
 void seed_rng(T& rng)
 {
@@ -43,34 +113,91 @@ void seed_rng(T& rng)
         rng.seed(tv.tv_sec*tv.tv_usec);
 }
 
-using namespace std;
-
-static
-void print_usage(char *pname, FILE *fp)
+void
+sample(size_t samples, size_t k)
 {
-        (void)fprintf(fp, "\nUsage: %s SAMPLES CARDINALITY A1 A2\n\n", pname);
-}
-
-int
-main(int argc, char *argv[])
-{
-        if (argc != 5) {
-                print_usage(argv[0], stderr);
-                exit(EXIT_FAILURE);
-        }
-        size_t n  = atoi(argv[1]);
-        size_t k  = atoi(argv[2]);
-        double a1 = atof(argv[3]);
-        double a2 = atof(argv[4]);
-
         boost::random::mt19937 gen; seed_rng(gen);
-        entropy_distribution_t<double, p_t> dist(k, a1, a2);
+        entropy_distribution_t<double, p_t> d(k, options.a1, options.a2);
 
-        for (size_t i = 0; i < n; i++) {
-                cout << dist(gen, 0.1)
+        if (options.print_entropy) {
+                for (size_t i = 0; i < samples; i++) {
+                        cout << entropy(d(gen, options.variance))
+                             << endl;
+                }
+        }
+        else {
+                for (size_t i = 0; i < samples; i++) {
+                        cout << d(gen, options.variance)
+                             << endl;
+                }
+        }
+        if (options.verbose) {
+                cerr << "acceptance ratio: "
+                     << d.acceptance_ratio()
                      << endl;
         }
-        cerr << "acceptance ratio: "
-             << dist.acceptance_ratio()
-             << endl;
+}
+
+// main function
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+        extern int optind;
+        vector<string> tokens;
+
+        for(;;) {
+                int c, option_index = 0;
+                static struct option long_options[] = {
+                        { "counts",               1, 0, 'a' },
+                        { "print-entropy",        0, 0, 'b' },
+                        { "variance",             1, 0, 'c' },
+                        { "help",                 0, 0, 'h' },
+                        { "version",              0, 0, 'x' },
+                        { 0,                      0, 0,  0  }
+                };
+
+                c = getopt_long(argc, argv, "v",
+                                long_options, &option_index);
+
+                if(c == -1) {
+                        break;
+                }
+
+                switch(c) {
+                case 'a':
+                        tokens = token(string(optarg), ':');
+                        if (tokens.size() != 2) {
+                                wrong_usage(argv[0], "Invalid number of pseudocounts.");
+                        }
+                        options.a1 = atof(tokens[0].c_str());
+                        options.a2 = atof(tokens[1].c_str());
+                        break;
+                case 'b':
+                        options.print_entropy = true;
+                        break;
+                case 'c':
+                        options.variance = atof(optarg);
+                        break;
+                case 'v':
+                        options.verbose = true;
+                        break;
+                case 'h':
+                        print_usage(argv[0], stdout);
+                        exit(EXIT_SUCCESS);
+                case 'x':
+                        print_version(stdout);
+                        exit(EXIT_SUCCESS);
+                default:
+                        wrong_usage(argv[0], NULL);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        if(optind+2 != argc) {
+                wrong_usage(argv[0], "Wrong number of arguments.");
+                exit(EXIT_FAILURE);
+        }
+        sample(atoi(argv[optind]), atoi(argv[optind+1]));
+
+        return 0;
 }
